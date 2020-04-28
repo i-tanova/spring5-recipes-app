4d871562c72d9f8e1dc522b027c0745b07f0f95e
ivana.tanova <tanovait@gmail.com>
2020-04-28 09:28:53 +0300

    Ingredients - One to many added

---
+++ src/main/java/com/ivana/tanova/recipesspringapp/domain/Notes.java
@@ -0,0 +1,38 @@
+package com.ivana.tanova.recipesspringapp.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Notes {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Lob
+    private String recipeNotes;
+    @OneToOne
+    private Recipe recipe;
+
+    public String getRecipeNotes() {
+        return recipeNotes;
+    }
+
+    public void setRecipeNotes(String recipeNotes) {
+        this.recipeNotes = recipeNotes;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
